9ead86b4e1af16aba3cff51ff9633f05487a5a0e
Catherine Chiu <chiuycatherine@gmail.com>
2021-11-15 22:12:35 -0800

    LineUp function works

--- src/init.js
+++ src/init.js
@@ -22,12 +22,65 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    window.dancers.push(dancer);
+
     $('body').append(dancer.$node);
   });
+
+  $('.addLineUpButton').on('click', function(event) {
+    // for (let )
+    Dancer.prototype.lineUp();
+  });
+  // $('.addPepeButton').on('click', function(event) {
+
+  //   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+
+  //   // get the maker function for the kind of dancer we're supposed to make
+  //   var dancerMakerFunction = window[dancerMakerFunctionName];
+
+  //   // make a dancer with a random position
+
+  //   var dancer = new dancerMakerFunction(
+  //     $("body").height() * Math.random(),
+  //     $("body").width() * Math.random(),
+  //     Math.random() * 1000
+  //   );
+  //   $('body').append(dancer.$node);
+  //   $(dancer.$node).hover(makeBigger, returnToOriginalSize);
+
+  //   window.dancers.push(dancer);
+
+  // });
+
+
+  $(document).scroll(function(event) {
+
+    /* dancerMakerFunctionName is a string which must match
+     * one of the dancer maker functions available in global scope.
+     * A new object of the given type will be created and added
+     * to the stage.
+     */
+    var dancerMakerFunctionName1 = "NotADancer"
+    console.log(dancerMakerFunctionName1)
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction1 = window[dancerMakerFunctionName1];
+
+    // make a dancer with a random position
+
+    var dancer1 = new dancerMakerFunction1(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(dancer1.$node);
+    window.dancers.push(dancer);
+  });
 });
+
 
